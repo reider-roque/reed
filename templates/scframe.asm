e1ff0b45537e01ef3fb9c9caa32cb7931f01ff58
Oleg Mitrofanov <it@unlim.xyz>
2015-09-26 08:57:32 -0400

    Add comments

--- templates/scframe.asm
+++ templates/scframe.asm
@@ -1,16 +1,17 @@
-; Shellcode testing frame written in pure assembly
+; Shellcode testing frame written in pure Assembly
+; Author: Oleg Mitrofanov (reidre-roque) 2015
 ;
 ; Remember to link with ld -z execstack!
+;
 ; In gdb set breakpoint at scexec label, the call instruction after that
 ; label will jump execution to the shellcode. To dissassemble then use 
 ; `disas $eip,+20`
 ;
-; Author: Oleg Mitrofanov (reidre-roque) 2015
 
 section .data
     shellcode_ascii: db "\xSH\xEL\xLC\xOD\xEE" ; SHELLCODE GOES HERE
-
     shellcode_len:   equ $-shellcode_ascii
+
     len_err_msg: db "Error: Shellcode shellcode_len must be a multiple of 4 (it wasn't).", 10
     len_err_len: equ $-len_err_msg
     dig_err_msg: db "Error: The following symbol is not valid hexadecimal digit: "
@@ -23,27 +24,31 @@ section .bss
 section .text
     global _start
 
-; Description: converts ASCII code to correspoindg hexadecimal digit
-; In:          ASCII digit code in EAX
-; Returns:     Single digit (0-9a-f) in EAX
-; Changes:     None
-; Reference:   ASCII 0-9 take 30-39 range in hex
-;              ASCII A-F take 41-46 range in hex
-;              ASCII a-f take 61-66 range in hex
-process_opcode_digit:
+; Description   : Converts ascii number to hexadecimal integer 
+;                 (StrToInt for hexadecimal numbers)
+; In            : ascii digit code in EAX
+; Returns       : Single hex digit (0-9a-f) in EAX
+; Changes       : None
+; Reference     : ascii 0-9 take 30-39 range in hex
+;                 ascii A-F take 41-46 range in hex
+;                 ascii a-f take 61-66 range in hex
+hex_ascii_to_int:
+    ; Check if ascii code is completely out of range
+    ; and then if it is a number in 0-9 range
     cmp eax, 0x30    
-    jb digit_error
+    jb symbol_error
     cmp eax, 0x66
-    ja digit_error
+    ja symbol_error
     cmp eax, 0x39
     ja .check_uppercase
-    ; It is a digit
+    ; It is a number 
     sub eax, 0x30   ; Convert ascii digit to hex digit
     ret
 
 .check_uppercase:
+    ; Check if ascii code is an uppercase letter in A-F range
     cmp eax, 0x41
-    jb digit_error
+    jb symbol_error
     cmp eax, 0x46
     ja .check_lowercase
     ; It is an uppercase
@@ -51,74 +56,82 @@ process_opcode_digit:
     ret
 
 .check_lowercase:
+    ; Check if ascii code is a lowercase letter in a-f range
     cmp eax, 0x61
-    jb digit_error
+    jb symbol_error
     ; It is a lowercase
     sub eax, 0x57     ; Convert ascii lowercase to hex digit
     ret
 
-_start:
-    mov eax, shellcode_len
-    mov ebx, 4
-    xor edx, edx    ; Clean DX to check for remainder
-    div bx          ; Quotient will be in AX, remainder in DX
-    cmp edx, 0
-    jnz len_error
 
+_start:
+    ; Check if shellcode length is multiple of 4
+    mov eax, shellcode_len
+    mov ebx, 4      ; Divisor
+    xor edx, edx    ; Clean DX for remainder
+    div bx          ; Quotient will be in AX, remainder in DX
+    cmp edx, 0      ; See if remainder is 0
+    jnz len_error   ; Jump to error meessage if not
+
+    ; Parse opcodes one by one converting their ascii representation
+    ; into hexadecimal integers
     mov esi, shellcode_ascii
     mov edi, shellcode_bin
     mov ecx, eax    ; Set the string length in dwords
 
 parse_opcode:
-    lodsd
-    shr eax, 16
-    mov ebx, eax    ; Copy ASCII value of the byte to ebx
-    shr eax, 8      ; Process first opcode byte digit in eax (high 8 bytes)
-    call process_opcode_digit
-    mov edx, eax    ; Store processed first digit in edx
+    lodsd           ; Copy next ascii opcode representation into EAX
+    shr eax, 16     ; Shed the '\x' part, leaving only ascii digits
+    mov ebx, eax    ; Copy ascii value of the byte to ebx
+    shr eax, 8      ; Process first ascii digit in eax (high 8 bytes)
+    call hex_ascii_to_int
+    push eax        ; Store first converted integer on the stack
 
-    and ebx, 0xFF   ; Process second opcode byte digit in ebx (low 8 bytes)
-    mov eax, ebx
-    call process_opcode_digit
+    and ebx, 0xFF   ; Process second ascii digit (low 8 bytes)
+    mov eax, ebx    ; Copy to eax
+    call hex_ascii_to_int
 
+    ; Note that lodsd loads the ascii text in reverse order. That is first
+    ; ascii digit is actually second integer digit and vice versa. Thus
+    ; we multiply the second processed ascii digit by 10, not the first one
     mov ebx, 0x10   ; Use ebx for multiplication
-    push edx        ; Store first digit on stack 
-    mul ebx         ; Multiply firt digit by 10
-    pop edx         ; Restore edx
-    add eax, edx    ; Add second digit to the first digit to get the end result
+    mul ebx         ; Multiply second ascii digit (now first integer digit) by 10
+    pop edx         ; Restore first converted digit (second integer) to edx
+    add eax, edx    ; Add second integer to the result of multiplication to get the end result
     
     stosb           ; Store the parsed opcode into the binary shellcode space
     loop parse_opcode
 
 scexec:    
     call shellcode_bin
-        
     jmp exit
 
+
 len_error:
-    mov eax, 4      ; write system call
+    ; Display error if shellcode length is not multiple of 4
+    mov eax, 4      ; write syscall
     mov ebx, 1      ; stdout
     mov ecx, len_err_msg
     mov edx, len_err_len 
     int 80h
     jmp exit
 
-; Display the invalid symbol found in the shellcode string
-digit_error:
-    push eax    ; eax at this point contains ASCII code of the invalid hex symbol
-    mov eax, 4
-    mov ebx, 1
+symbol_error:
+    ; Display the invalid symbol found in the shellcode string
+    push eax    ; eax at this point contains ascii code of the invalid hex symbol
+    mov eax, 4  ; write syscall
+    mov ebx, 1  ; stdout
     mov ecx, dig_err_msg
     mov edx, dig_err_len
     int 80h
     mov eax, 4
     mov ebx, 1
-    mov ecx, esp
+    mov ecx, esp ; The invalid symbol
     mov edx, 1
     int 80h
     mov eax, 4
     mov ebx, 1
-    mov ecx, eol
+    mov ecx, eol ; Newline char
     mov edx, 1
     int 80h
 
