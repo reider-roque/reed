78578900ad2cece4bc9bc08e1a0256e0f611045b
Oleg Mitrofanov <it@unlim.xyz>
2015-11-01 10:21:13 -0500

    Add Windows DOS hello world example

---
+++ templates/win-hello-dos.asm
@@ -0,0 +1,25 @@
+;
+; This program uses DOS interruptions and will not run under Windows 7 x64
+; It runs under Windows XP SP3 x86; I didn't check Windows 7 x86
+;
+; Build and link under Windows:
+;
+; nasm -f obj hello.asm
+; alink -c -oEXE hello.obj
+;
+
+bits 16
+
+section data
+    message db "Hello World!", 13, 10, "$"
+
+section text
+
+..start:
+    mov ax, data     ; Have data segment poin to the data section
+    mov ds, ax
+    mov dx, message  ; Load message address into dx
+    mov ah, 9        ; Write string to stdout function #
+    int 21h
+    mov ah, 4Ch      ; Exit function #
+    int 21h
